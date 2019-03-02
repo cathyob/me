f32827cd373a5344f143e0e6f62b159d09bff68b
Cathy OBrien <cathyobrien@Cathys-MBP.fios-router.home>
2019-03-02 13:55:23 -0500

    Reorganization of repo, create image slider

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 window.addEventListener("load", function() {
     const splash = document.querySelector("#splashHeader");
     // 1.5 seconds after window loads hide the splashHeader
-    setTimeout(function(){
+    setTimeout( function(){
         splash.classList.add("Swiped");
+        setTimeout( function() {
+            photoSlider();
+        }, 2000) // long enough to be after swiped class added and height transition
     }, 1500);
 });
 
+function photoSlider() {
+    const photoTimerSeconds = 2.5;
+    const photoSlides = document.getElementById('photoSlider').children;
+    let currentSlide = 0;
+    setInterval( function() {
+        photoSlides[currentSlide].classList.remove('Active');
+        currentSlide = (currentSlide + 1) % photoSlides.length; // using modulus allows it to loop once reaching end e.g. 7/7=1 so remainder is 0 so it goes to 1st slide
+        photoSlides[currentSlide].classList.add('Active');
+    }, photoTimerSeconds * 1000)
+}
 
 function submitForm() {
     const name = document.getElementById('name').value;
